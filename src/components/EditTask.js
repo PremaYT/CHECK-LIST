c5c03bd27b89cd7898a7d6ec9ada623f9ad41630
unknown <pytabareddi10@gmail.com>
2024-05-30 11:50:33 +0530

    build error fix

--- src/components/EditTask.js
+++ src/components/EditTask.js
@@ -1,35 +1,37 @@
-import React from 'react'
-import { Modal, Button } from 'react-bootstrap'
-import { useDispatch } from 'react-redux'
-import TaskForm from './TaskForm'
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import TaskForm from "./TaskForm";
 
-import { updateTask } from '../actions/taskActions'
+import { updateTask } from "../actions/taskActions";
 
 const EditTask = (props) => {
-    const {showModal, id, name, status, handleModalShow} = props
-    const dispatch =  useDispatch()
+  const { showModal, id, handleModalShow } = props;
+  const dispatch = useDispatch();
 
-    const editItem = (formData) => {
-        dispatch(updateTask(id, formData))
-        handleModalShow()
-    }
+  const editItem = (formData) => {
+    dispatch(updateTask(id, formData));
+    handleModalShow();
+  };
 
-    return (
-        <Modal
-            show={showModal}
-            {...props}
-            size="md"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            >
-            <Modal.Body>
-                <TaskForm {...props} editItem={editItem} />
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={handleModalShow} variant="danger">Close</Button>
-            </Modal.Footer>
-        </Modal>
-    )
-}
+  return (
+    <Modal
+      show={showModal}
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Body>
+        <TaskForm {...props} editItem={editItem} />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={handleModalShow} variant="danger">
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
-export default EditTask
+export default EditTask;
